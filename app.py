10ab9394d1e54ba726e0e9647b08ef24be702270
Jonathan Chang <jonathan.chang@optum.com>
2018-01-22 11:37:08 -0500

    test a few lines

--- app.py
+++ app.py
@@ -8,9 +8,29 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
+#####
+from flask import redirect
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+#####
+
 # Flask app should start in global layout
 app = Flask(__name__)
 
+#####
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
+#####
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
