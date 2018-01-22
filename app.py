8dd86cd491271c61de63a23715edca30b9e3baad
Jonathan Chang <jonathan.chang@optum.com>
2018-01-22 12:53:57 -0500

    create book class

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 #####
-# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
-#
-# db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 
-# class Book(db.Model):
-#     title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
-#
-#     def __repr__(self):
-#         return "<Title: {}>".format(self.title)
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+
+    def __repr__(self):
+        return "<Title: {}>".format(self.title)
 #####
 
 @app.route('/webhook', methods=['POST'])
