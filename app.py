74e8219dab63bdabdbdc441d7d422032a40a861c
Jonathan Chang <jonathan.chang@optum.com>
2018-01-22 14:21:30 -0500

    added app routing

--- app.py
+++ app.py
@@ -13,15 +13,15 @@ from flask import redirect
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
-#project_dir = os.path.dirname(os.path.abspath(__file__))
-#database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
 #####
 
 # Flask app should start in global layout
 app = Flask(__name__)
 
-#####
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+#######################################################
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file # os.environ['DATABASE_URL']
 
 db = SQLAlchemy(app)
 
@@ -44,7 +44,27 @@ def home():
             print(e)
     books = Book.query.all()
     return render_template("home.html", books=books)
-#####
+@app.route("/update", methods=["POST"])
+def update():
+    try:
+        newtitle = request.form.get("newtitle")
+        oldtitle = request.form.get("oldtitle")
+        book = Book.query.filter_by(title=oldtitle).first()
+        book.title = newtitle
+        db.session.commit()
+    except Exception as e:
+        print("Couldn't update book title")
+        print(e)
+    return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
+#######################################################
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
@@ -74,6 +94,20 @@ def makeWebhookResult(req):
 
     print("Response:")
     print(speech)
+
+    #######################################################
+    books = None
+    if request.form:
+        try:
+            book = zone
+            db.session.add(book)
+            db.session.commit()
+        except Exception as e:
+            print("Failed to add book")
+            print(e)
+    books = Book.query.all()
+    return render_template("home.html", books=books)
+    #######################################################
 
     return {
         "speech": speech,
